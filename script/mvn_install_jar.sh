92006b653bf8fa5dcd0862da462aff8b1937c726
ShiJ <jianlin.shi@va.gov>
2024-01-19 02:51:36 -0700

    run mvn install script before packaging

--- script/mvn_install_jar.sh
+++ script/mvn_install_jar.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
-mvn install:install-file -Dfile=../lib/annotationadmin-integration-1.0.13.jar -DgroupId=gov.va.vinci -DartifactId=annotation-admin -Dversion=1.0 -Dpackaging=jar
+mvn install:install-file -Dfile=lib/annotationadmin-integration-1.0.13.jar -DgroupId=gov.va.vinci -DartifactId=annotation-admin -Dversion=1.0 -Dpackaging=jar
 
-mvn install:install-file -Dfile=../lib/MultiSplit.jar -DgroupId=org.jdesktop.swingx \
+mvn install:install-file -Dfile=lib/MultiSplit.jar -DgroupId=org.jdesktop.swingx \
         -DartifactId=multi-split -Dversion=1.0 -Dpackaging=jar
 
-mvn install:install-file -Dfile=../lib/utsapi2_0.jar -DgroupId=gov.nih.nlm.uts \
+mvn install:install-file -Dfile=lib/utsapi2_0.jar -DgroupId=gov.nih.nlm.uts \
         -DartifactId=uts -Dversion=2.0 -Dpackaging=jar
